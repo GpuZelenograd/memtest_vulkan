e78bdac5a0107d2afc8f3530eaea39a9feaecfb2
Vasily Galkin <galkin-vv@ya.ru>
2022-09-27 02:45:56 +0300

    install rust into container enable building

--- .github/actions/build-in-docker/build-in-docker.sh
+++ .github/actions/build-in-docker/build-in-docker.sh
@@ -5,3 +5,6 @@ ls
 set
 cargo --list
 rustup target list
+cargo build --release --target x86_64-pc-windows-gnu
+cargo build --release --target x86_64-unknown-linux-gnu
+cargo build --release --target aarch64-unknown-linux-gnu
