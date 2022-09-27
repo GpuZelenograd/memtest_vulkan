45ad92abcaa70217dde2d84fa73cf3772126d1de
Vasily Galkin <galkin-vv@ya.ru>
2022-09-27 03:46:39 +0300

    use 1.64 without .0

--- .github/actions/build-in-docker/build-in-docker.sh
+++ .github/actions/build-in-docker/build-in-docker.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 export HOME=/root
 export PATH=$HOME/.cargo/bin:$PATH
-rustup default 1.64.0
-cargo --list
-rustup target list
+rustup default 1.64
 cargo build --release --target x86_64-pc-windows-gnu
 cargo build --release --target x86_64-unknown-linux-gnu
 cargo build --release --target aarch64-unknown-linux-gnu
