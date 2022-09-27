1a0e398df0acf2071c97b101cf5b0e6151d30bfc
Vasily Galkin <galkin-vv@ya.ru>
2022-09-27 04:18:57 +0300

    artifacts install cross-linker in docker

--- .github/actions/build-in-docker/build-in-docker.sh
+++ .github/actions/build-in-docker/build-in-docker.sh
@@ -5,3 +5,5 @@ rustup default 1.64
 cargo build --release --target x86_64-pc-windows-gnu
 cargo build --release --target x86_64-unknown-linux-gnu
 cargo build --release --target aarch64-unknown-linux-gnu
+tar cjf target/x86_64-pc-windows-gnu/release/x86_64-linux-memtest_vulkan-${GITHUB_REF_NAME}.tar.xz target/x86_64-unknown-linux-gnu/release/memtest_vulkan
+tar cjf target/x86_64-pc-windows-gnu/release/aarch64-linux-memtest_vulkan-${GITHUB_REF_NAME}.tar.xz target/aarch64-unknown-linux-gnu/release/memtest_vulkan
