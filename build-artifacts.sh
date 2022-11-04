ff99622155830aa71591fd7acfc381da1845336d
Vasily Galkin <galkin-vv@ya.ru>
2022-11-05 01:57:08 +0300

    extract build-artifacts script runnable in parallel from docker action

---
+++ build-artifacts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+GIT_REF_NAME=$1
+cargo build --release --target x86_64-pc-windows-gnu --target x86_64-unknown-linux-gnu --target aarch64-unknown-linux-gnu
+mkdir target/artifacts
+cp -vf target/x86_64-pc-windows-gnu/release/memtest_vulkan.exe target/artifacts/memtest_vulkan-${GIT_REF_NAME}.exe
+tar cJvf target/artifacts/x86_64-linux-memtest_vulkan-${GIT_REF_NAME}.tar.xz -C target/x86_64-unknown-linux-gnu/release memtest_vulkan
+tar cJvf target/artifacts/aarch64-linux-memtest_vulkan-${GIT_REF_NAME}.tar.xz -C target/aarch64-unknown-linux-gnu/release memtest_vulkan
