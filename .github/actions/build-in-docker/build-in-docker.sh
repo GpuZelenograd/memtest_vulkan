eca13476c15578a842ccd1cf65182073176df0af
Vasily Galkin <galkin.vv@yandex.kz>
2022-11-05 01:22:04 +0300

    use image with preinstalled cargo and cached crate index

--- .github/actions/build-in-docker/build-in-docker.sh
+++ .github/actions/build-in-docker/build-in-docker.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-export HOME=/root
-export PATH=$HOME/.cargo/bin:$PATH
-rustup default 1.64
-bash
 cargo build --release --target x86_64-pc-windows-gnu
 cargo build --release --target x86_64-unknown-linux-gnu
 cargo build --release --target aarch64-unknown-linux-gnu
