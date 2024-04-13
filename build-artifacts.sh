9a52b9a036fb2aea6fe6ffe955d6603c82f0b09b
Vasily Galkin <galkin.vv@yandex.kz>
2024-04-14 00:40:25 +0300

    Artifact naming in ci

--- build-artifacts.sh
+++ build-artifacts.sh
@@ -5,6 +5,6 @@
     cargo build --release --target x86_64-pc-windows-gnu --target x86_64-unknown-linux-gnu --target aarch64-unknown-linux-gnu
     mkdir -p target/artifacts
     cp -vf target/x86_64-pc-windows-gnu/release/memtest_vulkan.exe target/artifacts/memtest_vulkan-${GIT_REF_NAME}.exe
-    tar cJvf target/artifacts/DesktopLinux_X86_64-memtest_vulkan-${GIT_REF_NAME}.tar.xz -C target/x86_64-unknown-linux-gnu/release memtest_vulkan
-    tar cJvf target/artifacts/EmbeddedLinux_AARCH64-memtest_vulkan-${GIT_REF_NAME}.tar.xz -C target/aarch64-unknown-linux-gnu/release memtest_vulkan
+    tar cJvf target/artifacts/memtest_vulkan-${GIT_REF_NAME}_DesktopLinux_X86_64.tar.xz -C target/x86_64-unknown-linux-gnu/release memtest_vulkan
+    tar cJvf target/artifacts/memtest_vulkan-${GIT_REF_NAME}_EmbeddedLinux_AARCH64.tar.xz -C target/aarch64-unknown-linux-gnu/release memtest_vulkan
 )
