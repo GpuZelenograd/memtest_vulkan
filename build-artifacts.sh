d3e9c32f586ecf82d938fbebd282f2c4c0a3e411
Vasily Galkin <galkin-vv@ya.ru>
2025-09-13 03:44:51 +0300

    win7 target

--- build-artifacts.sh
+++ build-artifacts.sh
@@ -3,9 +3,10 @@
     set -xeu
     GIT_REF_NAME="${1:-${GITHUB_REF_NAME:-`date +%Y-%m-%dT%H-%M-%S`}}"
     GIT_REF_NAME="${GIT_REF_NAME/\//---}"
+    RUSTC_BOOTSTRAP=1 cargo build -Zbuild-std --release --target x86_64-win7-windows-gnu
     cargo build --release --target x86_64-pc-windows-gnu --target x86_64-unknown-linux-gnu --target aarch64-unknown-linux-gnu
     mkdir -p target/artifacts
-    cp -vf target/x86_64-pc-windows-gnu/release/memtest_vulkan.exe target/artifacts/memtest_vulkan-${GIT_REF_NAME}.exe
+    cp -vf target/x86_64-win7-windows-gnu/release/memtest_vulkan.exe target/artifacts/memtest_vulkan-${GIT_REF_NAME}.exe
     tar cJvf target/artifacts/memtest_vulkan-${GIT_REF_NAME}_DesktopLinux_X86_64.tar.xz -C target/x86_64-unknown-linux-gnu/release memtest_vulkan
     tar cJvf target/artifacts/memtest_vulkan-${GIT_REF_NAME}_EmbeddedLinux_AARCH64.tar.xz -C target/aarch64-unknown-linux-gnu/release memtest_vulkan
 )
