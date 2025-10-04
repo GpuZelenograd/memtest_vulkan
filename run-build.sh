ea3498173dd4e70084c55601b4c37f9dfb107eba
Vasily Galkin <galkin-vv@ya.ru>
2025-10-05 00:21:21 +0300

    ci: publish no-win7 binary

--- run-build.sh
+++ run-build.sh
@@ -8,6 +8,7 @@
     cargo build --release --target x86_64-pc-windows-gnu --target x86_64-unknown-linux-gnu --target aarch64-unknown-linux-gnu
     RUSTC_BOOTSTRAP=1 cargo build -Zbuild-std --release --target x86_64-win7-windows-gnu
     mkdir -p target/artifacts
+    cp -vf target/x86_64-pc-windows-gnu/release/memtest_vulkan.exe target/artifacts/memtest_vulkan-${GIT_REF_NAME}-nowin7.exe
     cp -vf target/x86_64-win7-windows-gnu/release/memtest_vulkan.exe target/artifacts/memtest_vulkan-${GIT_REF_NAME}.exe
     tar cJvf target/artifacts/memtest_vulkan-${GIT_REF_NAME}_DesktopLinux_X86_64.tar.xz -C target/x86_64-unknown-linux-gnu/release memtest_vulkan
     tar cJvf target/artifacts/memtest_vulkan-${GIT_REF_NAME}_EmbeddedLinux_AARCH64.tar.xz -C target/aarch64-unknown-linux-gnu/release memtest_vulkan
