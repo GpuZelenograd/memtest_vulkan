eac4a7b670514e7977db22fd6ffb8e8fdfd9f6ac
Vasily Galkin <galkin-vv@ya.ru>
2025-12-04 15:24:01 +0300

    Switch to a builder image with rust-lld-based linkers
    Should fix https://github.com/GpuZelenograd/memtest_vulkan/issues/71

--- run-build.sh
+++ run-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 (
-    # getting all below targets to work may be a bit tricky, especially x86_64-win7-windows-gnu,
+    # getting all below targets to link may be a bit tricky, especially x86_64-win7-windows-gnu,
     # so prepared Docker builder image provided at https://github.com/galkinvv/manycross2014/pkgs/container/manycross2014
     set -xeu
     GIT_REF_NAME="${1:-${GITHUB_REF_NAME:-`date +%Y-%m-%dT%H-%M-%S`}}"
@@ -8,7 +8,7 @@
     cargo build --release --target x86_64-pc-windows-gnu --target x86_64-unknown-linux-gnu --target aarch64-unknown-linux-gnu
     RUSTC_BOOTSTRAP=1 cargo build -Zbuild-std --release --target x86_64-win7-windows-gnu
     mkdir -p target/artifacts
-    cp -vf target/x86_64-pc-windows-gnu/release/memtest_vulkan.exe target/artifacts/memtest_vulkan-${GIT_REF_NAME}-nowin7.exe
+    cp -vf target/x86_64-pc-windows-gnu/release/memtest_vulkan.exe target/artifacts/memtest_vulkan-${GIT_REF_NAME}-alt-experimental.exe
     cp -vf target/x86_64-win7-windows-gnu/release/memtest_vulkan.exe target/artifacts/memtest_vulkan-${GIT_REF_NAME}.exe
     tar cJvf target/artifacts/memtest_vulkan-${GIT_REF_NAME}_DesktopLinux_X86_64.tar.xz -C target/x86_64-unknown-linux-gnu/release memtest_vulkan
     tar cJvf target/artifacts/memtest_vulkan-${GIT_REF_NAME}_EmbeddedLinux_AARCH64.tar.xz -C target/aarch64-unknown-linux-gnu/release memtest_vulkan
